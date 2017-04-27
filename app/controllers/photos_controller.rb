0123a32bf1afda64188e659ca156d47857dcfb66
mattbey <matt.bey@gmail.com>
2017-04-26 23:03:14 -0500

    4/26 attempt

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,38 @@
+class PhotosController < ApplicationController
+
+  def index
+    @list_of_photos=Photo.all
+    render("photos/index.html.erb")
+  end
+  def show
+    @photo = Photo.find(params[:id])
+    render("photos/show.html.erb")
+  end
+  def new_form
+    render("photos/new_form.html.erb")
+  end
+  def edit_form
+    @photo = Photo.find(params[:id])
+    render("photos/edit_form.html.erb")
+  end
+  def update_row
+    p = Photo.find(params[:id])
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("/photos/#{p.id}")
+  end
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("/photos")
+  end
+def delete_row
+  p = Photo.find(params[:id])
+p.delete
+redirect_to("/photos")
+end
+
+end
